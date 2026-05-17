1b41758b5224d0a0010fd8dd5c4c77dab5a80b07
Pranto800 <kaowsarazad33@gmail.com>
2026-05-18 02:08:37 +0600

    fast push nev foot and other section add

---
+++ src/lib/pet/data.js
@@ -0,0 +1,15 @@
+ export const fetchPets = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/courses`);
+
+  const data = await res.json();
+
+  return data;
+};
+
+ export const fetchLimitPetCard = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/card`);
+
+  const data = await res.json();
+
+  return data;
+};
