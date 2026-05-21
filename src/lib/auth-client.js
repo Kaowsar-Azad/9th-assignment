c95486292173d0e4fbefd53ae57f91894e9faf72
Pranto800 <kaowsarazad33@gmail.com>
2026-05-21 18:51:33 +0600

    update code

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { jwt } from "better-auth/plugins"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL ||  process.env.NEXT_PUBLIC_BETTER_AUTH_URL ,
      plugins: [
         jwt(), 
     ]
